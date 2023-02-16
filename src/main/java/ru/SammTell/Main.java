d88c7aca894b97f6add2f3b4fe00e7da27a5f3f6
Samm <86586449+TellSamm@users.noreply.github.com>
2023-02-16 23:17:05 +0300

    Update Main.java

--- src/main/java/ru/SammTell/Main.java
+++ src/main/java/ru/SammTell/Main.java
@@ -5,13 +5,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int n = 1;
-        if(n == 1) {
-        System.out.println("Hello World Of Warcraft !");
+        int n = 3;
+        if(n == 3) {
+        System.out.println("Hello World Of Warcraft3 !");
         }else{
-            System.out.println("This is not warcraft");
+            System.out.println("This is not warcraft3");
         }
-        // в любом случае выведем
+        // в любом случае выведем!
         System.out.println("Hello Samm");
         System.out.println("Hello World Of Warcraft!!!!");
     }
