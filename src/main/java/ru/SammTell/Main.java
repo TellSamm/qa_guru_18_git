f92c2603f797664ff1cf16b247f762e693b1eb6a
TellSamm <chernikov.semen21@gmail.com>
2023-02-16 14:28:38 +0300

    conditional operator if else add

--- src/main/java/ru/SammTell/Main.java
+++ src/main/java/ru/SammTell/Main.java
@@ -1,9 +1,18 @@
 package ru.SammTell;
 
 public class Main {
-    public static void main(String[] args)
-    {
+
+
+    public static void main(String[] args) {
+
+        int n = 1;
+        if(n == 1) {
         System.out.println("Hello World Of Warcraft !");
+        }else{
+            System.out.println("This is not warcraft");
+        }
+
+
     }
 
 }
