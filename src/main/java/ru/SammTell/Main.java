814feab8292696e404ab73599da108888db5df63
Samm <86586449+TellSamm@users.noreply.github.com>
2023-02-16 14:26:35 +0300

    Update Main.java

--- src/main/java/ru/SammTell/Main.java
+++ src/main/java/ru/SammTell/Main.java
@@ -3,6 +3,7 @@ package ru.SammTell;
 public class Main {
     public static void main(String[] args)
     {
+        System.out.println("Hello Samm");
         System.out.println("Hello World Of Warcraft!!!!");
     }
 
